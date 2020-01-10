9310ea45849c1abc0faf6e839f16f74ed34cbe85
Fabrice Brito <fabrice.brito@terradue.com>
2020-01-10 11:17:19 +0100

    fixed wheel copy (one per Python version)

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,10 +4,12 @@ export JDK_HOME=/usr/lib/jvm/java-1.8.0-openjdk.x86_64
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk.x86_64
 export LD_LIBRARY_PATH=$JDK_HOME/jre/lib/amd64/server/:/opt/anaconda/lib/:$LD_LIBRARY_PATH
 
-$PYTHON setup.py bdist_wheel
+$PYTHON setup.py sdist bdist_wheel
 
-# Add more build steps here, if they are necessary.
+mkdir -p $PREFIX/jpy_wheel
 
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+echo $PY_VER > /tmp/p_ver.txt
+
+VER=$( echo $PY_VER | tr -d '.' )
+
+cp -v $( find $SRC_DIR -name "jpy*$VER*$VER*.whl" ) $PREFIX/jpy_wheel 
