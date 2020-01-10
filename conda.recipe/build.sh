515391cfc267ba1dc3394fc821221c678520e223
Fabrice Brito <fabrice.brito@terradue.com>
2020-01-10 08:51:48 +0100

    update to build wheel for snappy

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,7 +4,7 @@ export JDK_HOME=/usr/lib/jvm/java-1.8.0-openjdk.x86_64
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk.x86_64
 export LD_LIBRARY_PATH=$JDK_HOME/jre/lib/amd64/server/:/opt/anaconda/lib/:$LD_LIBRARY_PATH
 
-$PYTHON setup.py install
+$PYTHON setup.py bdist_wheel
 
 # Add more build steps here, if they are necessary.
 
